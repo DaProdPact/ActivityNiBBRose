16ea5448fe3eb082d335146afbe76b9852bb1a81
Arjhennnnnn <madlangsakay.arjhen05@gmail.com>
2023-05-27 15:34:24 +0800

    First Activity

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// Create routes that the user navigate in this page
+
+// /                 Redirect to index
+// /user         Redirect to users page
+// /about       Redirect to about
+// /supervisor
+// /employee     This route should redirect also to users page
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('activity1.index');
+});
+Route::get('/user', function () {
+    return view('activity1.user');
+});
+Route::get('/about', function () {
+    return view('activity1.about');
+});
+Route::get('/employee', function () {
+    return view('activity1.user');
 });
